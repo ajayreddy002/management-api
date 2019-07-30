96872ff4715369bcdcb0f3f1dc77a840bea6e0d4
Ajay Kumar Reddy  Sanivarapu <asanivarapu@knowledgeinfotech.com>
2019-07-30 18:53:39 +0530

    Added token and bcrypt

--- controllers/adminLoginController.js
+++ controllers/adminLoginController.js
@@ -1,29 +1,53 @@
 const loginModal = require('../models').adminLogin;
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+var jwt = require('jsonwebtoken');
+const saltRounds = 10;
 module.exports = {
-    create:(req, res) => {
-        if(req.body.email, req.body.password) {
-            loginModal.create({
-                email: req.body.email,
-                password: req.body.password
-            }).then(data => {
-                res.send('Inserted');
-            }).catch(err => {
-                res.send('Not inserted');
+    create: (req, res) => {
+        if (req.body.email, req.body.password) {
+            bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+                console.log(hash)
+                loginModal.create({
+                    email: req.body.email,
+                    password: hash
+                }).then(data => {
+                    console.log(data.id)
+                    res.send('Inserted');
+                }).catch(err => {
+                    // console.log(err)
+                    res.send('Not inserted');
+                });
             });
         }
     },
-    index:(req, res) => {
+    index: (req, res) => {
         if (req.body.email, req.body.password) {
             loginModal.findAll({
                 where: {
                     email: req.body.email
                 }
             }).then(data => {
-                res.send(data);
+                console.log(data[0].password)
+                bcrypt.compare(req.body.password, data[0].password, function (err, result) {
+                    if (result === true) {
+                        console.log(result);
+                        payload = {email: req.body.email};
+                        options = {expiresIn: '2h' };
+                        iat = Math.floor(Date.now() / 1000) - 30
+                        secret = process.env.private_key;
+                        const token = jwt.sign(payload, secret, options, iat);
+                        res.send({res: token});
+                    } else {
+                        console.log(err, 'err');
+                        res.send('Invalid Password', 401)
+                    }
+                });
             }).catch(err => {
                 console.log(err);
+                res.send('Enter Valid details', 401);
             })
-        } else {    
+        } else {
             res.send('Enter Valid details');
         }
     }
